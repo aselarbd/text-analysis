06b4fb8d091613c22e38e735f7ea5d998609918d
aselarbd <asela.dasanayaka@tum.de>
2020-02-27 14:17:07 +0100

    Added full policy loading
    Issue #10

--- mosega_frontend/src/constants/URL.js
+++ mosega_frontend/src/constants/URL.js
@@ -1,2 +1,3 @@
 export const ADD_NEW_POLICY = '/PrivacyPolicyAPI/api/v1/PrivacyPolicy/';
+export const GET_ONE_POLICY = '/PrivacyPolicyAPI/api/v1/PrivacyPolicy';
 export const BASE_URL = 'http://127.0.0.1:8000';
