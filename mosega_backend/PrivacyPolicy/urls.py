850b8473ac263e5dfbdf3bb8ba174a70d0ddaf88
aselarbd <asela.dasanayaka@tum.de>
2020-02-28 01:28:57 +0100

    quick-fix for backend request adjustments #10

--- mosega_backend/PrivacyPolicy/urls.py
+++ mosega_backend/PrivacyPolicy/urls.py
@@ -7,9 +7,9 @@ configs = ConfigHandler.load_config('config.yaml')
 
 
 API_PATH = configs['api']['backend']['Policy']['view'] + '/' +configs['api']['title'] + '/' +\
-           configs['api']['current_version'] + '/' + configs['api']['backend']['Policy']['name'] + '/'
+           configs['api']['current_version'] + '/' + configs['api']['backend']['Policy']['name']
 
 urlpatterns = [
     path(API_PATH, views.PrivacyPolicyList.as_view()),
-    path(API_PATH + '<int:ID>/', views.PrivacyPolicyDetails.as_view()),
+    path(API_PATH + '/<int:ID>', views.PrivacyPolicyDetails.as_view()),
 ]
--- mosega_backend/TermsAndConditions/urls.py
+++ mosega_backend/TermsAndConditions/urls.py
@@ -7,9 +7,9 @@ configs = ConfigHandler.load_config('config.yaml')
 
 
 API_PATH = configs['api']['backend']['Term']['view'] + '/' +configs['api']['title'] + '/' +\
-           configs['api']['current_version'] + '/' + configs['api']['backend']['Term']['name'] + '/'
+           configs['api']['current_version'] + '/' + configs['api']['backend']['Term']['name']
 
 urlpatterns = [
     path(API_PATH, views.TermsAndConditionsList.as_view()),
-    path(API_PATH + '<int:ID>/', views.TermsAndConditionsDetails.as_view()),
+    path(API_PATH + '/<int:ID>', views.TermsAndConditionsDetails.as_view()),
 ]
