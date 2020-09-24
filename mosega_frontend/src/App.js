6a44cde77b41f28b2122e21da00072e1ee79052d
aselarbd <rukmal.tb@gmail.com>
2020-09-24 11:42:47 +0200

    implemented feature # 21

--- mosega_frontend/src/App.js
+++ mosega_frontend/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import PrivacyPolicy from './containers/PrivacyPolicy/PrivacyPolicy';
 import TermAndCondition from './containers/TermAndCondition/TermAndCondition';
 import Process from './containers/Process/Process';
+import PreCluster from "./components/ProcessMain/PreCluster/PreCluster";
 
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
                       <Route path="/policy" exact component={PrivacyPolicy}/>
                       <Route path="/term" exact component={TermAndCondition}/>
                       <Route path="/process" exact component={Process}/>
+                      <Route path="/pre-cluster" exact component={PreCluster}/>
                       <Redirect from='/' to='/policy' />
                   </Switch>
 
